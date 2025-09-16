288157626b7050306d4da5f8c04d003db253c90f
maurizio-dominici <mauriziodominici4@gmail.com>
2025-09-16 13:37:45 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,34 @@
+# Snack 1 - Filtra e Modifica
+
+- Crea una funzione che somma due numeri.
+
+- Crea un array (longBooks) con i libri che hanno più di 300 pagine;
+
+- Creare un array (longBooksTitles) che contiene solo i titoli dei libri contenuti in longBooks.
+
+- Stampa in console ogni titolo nella console.
+
+# Snack 2 - Il primo libro scontato
+
+- Creare un array (availableBooks) che contiene tutti i libri disponibili.
+
+- Crea un array (discountedBooks) con gli availableBooks, ciascuno con il prezzo scontato del 20% (mantieni lo stesso formato e arrotonda al centesimo)
+
+- Salva in una variabile (fullPricedBook) il primo elemento di discountedBooks che ha un prezzo intero (senza centesimi).
+
+# Snack 3 - Ordinare gli Autori
+
+- Creare un array (authors) che contiene gli autori dei libri.
+
+- Crea una variabile booleana (areAuthorsAdults) per verificare se gli autori sono tutti maggiorenni.
+
+- Ordina l’array authors in base all’età, senza creare un nuovo array.
+  (se areAuthorsAdult è true, ordina in ordine crescente, altrimenti in ordine decrescente)
+
+# Snack 4 - Calcola l’età media
+
+- Creare un array (ages) che contiene le età degli autori dei libri.
+
+- Calcola la somma delle età (agesSum) usando reduce.
+
+- Stampa in console l’età media degli autori dei libri.
---
+++ main.js
@@ -0,0 +1,91 @@
+const books = [
+  {
+    title: "React Billionaire",
+    pages: 250,
+    author: {
+      name: "Alice",
+      age: 35,
+    },
+    available: false,
+    price: "101€",
+    tags: ["advanced", "js", "react", "senior"],
+  },
+  {
+    title: "Advanced JS",
+    pages: 500,
+    author: {
+      name: "Bob",
+      age: 20,
+    },
+    available: true,
+    price: "25€",
+    tags: ["advanced", "js", "mid-senior"],
+  },
+  {
+    title: "CSS Secrets",
+    pages: 320,
+    author: {
+      name: "Alice",
+      age: 17,
+    },
+    available: true,
+    price: "8€",
+    tags: ["html", "css", "junior"],
+  },
+  {
+    title: "HTML Mastery",
+    pages: 200,
+    author: {
+      name: "Charlie",
+      age: 50,
+    },
+    available: false,
+    price: "48€",
+    tags: ["html", "advanced", "junior", "mid-senior"],
+  },
+];
+
+// const sommaNumeri = (a, b) => a + b;
+
+// const longBooks = books.filter((b) => b.pages > 300);
+// console.log(longBooks);
+
+// const longBooksTitles = longBooks.map((b) => b.title);
+// // console.log(longBooksTitles);
+
+// longBooksTitles.forEach((t) => console.log(t));
+
+// const availableBooks = books.filter((b) => b.available);
+// console.log(availableBooks);
+
+// const discountedBooks = availableBooks.map((aB) => {
+//   const price = parseFloat(aB.price.replace("€", ""));
+//   const discountedPrice = (price * 0.8).toFixed(2);
+//   return { ...aB, price: `${discountedPrice}€` };
+// });
+// console.log(discountedBooks);
+
+// const fullPricedBook = discountedBooks.find((db) => {
+//   const price = parseFloat(db.price.replace("€", ""));
+//   return price % 1 === 0;
+// });
+// console.log("fullPricedBook :", fullPricedBook);
+
+const authors = books.map((book) => book.author);
+console.log("authors:", authors);
+
+const areAuthorsAdults = authors.every((a) => a.age >= 18);
+console.log("areAuthorsAdults :", areAuthorsAdults);
+
+authors.sort((a, b) => b.age - a.age);
+
+const ages = authors.map((a) => a.age);
+console.log("ages :", ages);
+
+const agesSum = ages.reduce((acc, age) => {
+  return acc + age;
+}, 0);
+
+console.log(agesSum);
+
+console.log(`Eta media :, ${agesSum / ages.length}`);
